5f3838f6a27cbb790eacc5ebe3c82ecd1a49b603
Your Namecd <danielshaw111693@gmail.com>
2024-03-14 21:19:18 -0500

    configure for hosting

--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ const app = express();
 const salt = bcrypt.genSaltSync(10);
 const secret = process.env.SECRET;
 
-app.use(express.json());
 app.use(cors({ credentials: true, origin: "https://book-listing-app.onrender.com" }));
+app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
