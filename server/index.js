76cc889f8867a3eb7cad1e4e63ffead04019943a
Your Namecd <danielshaw111693@gmail.com>
2024-03-28 20:03:08 -0500

    configure for hosting

--- server/index.js
+++ server/index.js
@@ -26,10 +26,7 @@ const salt = bcrypt.genSaltSync(10);
 const secret = process.env.SECRET;
 
 const db = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DB_URI
 });
 
 
