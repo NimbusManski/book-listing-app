0107fec7c2bb6deec9ee6a0761bb0e1f3386b56b
Your Namecd <danielshaw111693@gmail.com>
2024-04-09 20:38:26 -0500

    configure for hosting

---
+++ client/src/pages/Login.js
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import axios from "axios";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  async function loginUser(e) {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_SERVER_URL}/login`,
+        {
+          username: username,
+          password: password,
+        },
+        {
+          withCredentials: true,
+          headers: {'Content-Type': 'application/json'},
+        }
+      );
+      // const response = await fetch(
+      //   `${process.env.REACT_APP_SERVER_URL}/login`,
+      //   {
+      //     method: "POST",
+      //     headers: { "Content-Type": "application/json"},
+      //     body: JSON.stringify({
+      //       username: username,
+      //       password: password,
+      //     }),
+      //     credentials: 'include'
+      //   }
+      // );
+      if (response.status === 200) {
+        setRedirect(true);
+      }
+    } catch (err) {
+      if (err.response.status === 401) {
+        alert("Invalid username or password");
+      }
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <div className="form-container">
+      <form className="form" onSubmit={loginUser}>
+        <h1>Login</h1>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button>Login</button>
+        Don't have an account? <Link to={"/register"}>Register here</Link>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
