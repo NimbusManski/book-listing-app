569e734adf483ba3f2fa833345fced4c2fb17ae4
Your Namecd <danielshaw111693@gmail.com>
2024-04-09 22:54:38 -0500

    configure for hosting

--- server/index.js
+++ server/index.js
@@ -145,6 +145,9 @@ app.get("/profile", (req, res) => {
           res.status(500).json({ message: "Internal server error" });
         }
       } else {
+        res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate'); // HTTP 1.1.
+  res.setHeader('Pragma', 'no-cache'); // HTTP 1.0.
+  res.setHeader('Expires', '0'); // Proxies.
         res.json(info);
       }
     });
