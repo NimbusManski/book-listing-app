cd411e5ee710b0fc40fb8628d7a1286241249334
Your Namecd <danielshaw111693@gmail.com>
2024-04-12 13:13:06 -0500

    configure for hosting

--- client/src/Header.js
+++ client/src/Header.js
@@ -54,6 +54,7 @@ const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/logout`, 
         if (response.status === 200) {
           // setIsLoggedIn(false);
           localStorage.clear();
+          document.cookie = '';
           setUserInfo({});
           navigate("/login");
         }
