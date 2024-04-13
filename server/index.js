3e3cd5a928c93d109cea48012712968a6887026b
Your Namecd <danielshaw111693@gmail.com>
2024-04-12 19:06:58 -0500

    configure for hosting

--- server/index.js
+++ server/index.js
@@ -285,7 +285,7 @@ app.delete("/profile/:id", (req, res) => {
 
 app.post("/logout", (req, res) => {
   try {
-    res.clearCookie("token").json("cookie deleted");
+    res.clearCookie("token", { domain: "https://book-listing-app.onrender.com" }).json("cookie deleted");
   } catch(err) {
     console.error(err);
   }
