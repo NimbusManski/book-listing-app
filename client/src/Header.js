e1cdfbdb4aa9f9e981cebd71020248aeb11b9151
Your Namecd <danielshaw111693@gmail.com>
2024-04-13 20:26:47 -0500

    configure for hosting

--- client/src/Header.js
+++ client/src/Header.js
@@ -59,7 +59,7 @@ const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/logout`, 
           setUserInfo({});
           window.localStorage.clear(); 
           window.sessionStorage.clear();
-          document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
+          console.log(document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/');
           // setIsLoggedIn(false);
           navigate("/login");
         }
